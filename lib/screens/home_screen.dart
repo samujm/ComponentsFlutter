93e8596e5f384cc3a0934c22629c50577b439cfc
Samantha González <samujm76@gmail.com>
2022-10-23 00:17:17 -0500

    Navegar a una nueva pantalla (Con una ruta que no existe)

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:fl_components/screens/screens.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -14,7 +15,16 @@ class HomeScreen extends StatelessWidget {
             itemBuilder: (context, index) => ListTile(
                   leading: const Icon(Icons.access_time_outlined),
                   title: const Text('Nombre de Ruta'),
-                  onTap: () {},
+                  onTap: () {
+                    // final route = MaterialPageRoute(
+                    //     builder: (context) => const Listview1Screen(),
+                    //     );
+
+                    // // Navigator.pushReplacement(context, route);
+                    // Navigator.push(context, route);
+
+                    Navigator.pushNamed(context, 'card2');
+                  },
                 ),
             separatorBuilder: (_, __) => const Divider(),
             itemCount: 100));

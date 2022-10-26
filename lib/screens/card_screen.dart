a1b47fba3ce2626be17dac05af0034bdb495dc89
Samantha González <samujm76@gmail.com>
2022-10-26 18:00:33 -0500

    Argumentos a los Widgets, Colocar imagenes que se mandan y validar nombre como parametro

--- lib/screens/card_screen.dart
+++ lib/screens/card_screen.dart
@@ -16,11 +16,22 @@ class CardScreen extends StatelessWidget {
           children: const [
             CustomCardType1(),
             SizedBox(height: 10),
-            CustomCardType2(),
-            SizedBox(height: 10),
-            CustomCardType2(),
-            SizedBox(height: 10),
-            CustomCardType2(),
+            CustomCardType2(
+              imageUrl:
+                  'https://honeysanime.com/wp-content/uploads/2015/02/5-Centimeters-Per-Second-03.jpg',
+              name: 'Flower Sakura',
+            ),
+            SizedBox(height: 20),
+            CustomCardType2(
+              imageUrl:
+                  'https://wallpapers.com/images/hd/japanese-castle-anime-landscape-r217nepmbm4j1hci.jpg',
+            ),
+            SizedBox(height: 20),
+            CustomCardType2(
+              imageUrl:
+                  'https://wallpaperforu.com/wp-content/uploads/2021/04/Wallpaper-Anime-Landscape-Neon-Colorful-3840x2160px-4k50-scaled.jpg',
+              name: 'Tokyo in the night',
+            ),
             SizedBox(height: 100),
           ],
         ));
--- lib/widgets/custom_card_type_2.dart
+++ lib/widgets/custom_card_type_2.dart
@@ -2,7 +2,14 @@ import 'package:flutter/material.dart';
 import 'package:fl_components/themes/app_theme.dart';
 
 class CustomCardType2 extends StatelessWidget {
-  const CustomCardType2({Key? key}) : super(key: key);
+  final String imageUrl;
+  final String? name;
+
+  const CustomCardType2({
+    Key? key,
+    required this.imageUrl,
+    this.name,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -15,19 +22,20 @@ class CustomCardType2 extends StatelessWidget {
       shadowColor: AppTheme.primary.withOpacity(0.5),
       child: Column(
         children: [
-          const FadeInImage(
-            image: NetworkImage(
-                'https://www.namesnack.com/images/Namesnack-nombres-para-compa%C3%B1%C3%ADas-de-landscape-2400x1600-2021084.jpeg?crop=16:9,smart&width=1200&dpr=2'),
-            placeholder: AssetImage('assets/img/jar-loading.gif'),
+          FadeInImage(
+            image: NetworkImage(imageUrl),
+            placeholder: const AssetImage('assets/img/jar-loading.gif'),
             width: double.infinity,
             height: 230,
             fit: BoxFit.cover,
-            fadeInDuration: Duration(milliseconds: 300),
+            fadeInDuration: const Duration(milliseconds: 300),
           ),
-          Container(
+          if (name != null)
+            Container(
               alignment: AlignmentDirectional.centerEnd,
               padding: const EdgeInsets.only(right: 20, top: 10, bottom: 10),
-              child: const Text('Un hermoso paisaje')),
+              child: Text(name!),
+            ),
         ],
       ),
     );
